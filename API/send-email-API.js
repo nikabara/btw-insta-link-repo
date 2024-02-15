068d274dee3b069e274a94a5ef7abfc6b125c24c
nikabara <63517031+nikabara@users.noreply.github.com>
2024-02-15 19:49:04 +0400

    Update send-email-API.js

--- API/send-email-API.js
+++ API/send-email-API.js
@@ -2,10 +2,10 @@ export const sendEmail = (header, message) => {
     Email.send
     ({
         Host : "smtp.elasticemail.com",
-        Username : "nikobar3005@gmail.com",
-        Password : "3E95B6DB198797D0216F5740888979ACFA7F",
-        To : "bythewayapp07@gmail.com",
-        From : "nikobar3005@gmail.com",
+        Username : "example@gmail.com",
+        Password : "5640ASD5A6S4D0A65ASD40S5",
+        To : "TOEXAMPLE@gmail.com",
+        From : "example@gmail.com",
         Subject : header,
         Body : message
     })
